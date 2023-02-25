27382193e00c560034f227ca73a24a266a32e861
SergeySokolov54 <116427633+SergeySokolov54@users.noreply.github.com>
2023-02-25 18:41:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,11 @@
 // Copyright 2021 NNTU-CS
 
 int gcd(int a, int b) {
-    // поместить сюда текст реализации алгоритма (на С++)
+    int k = 0;
+    for (int i = 1; i < a; i++) {
+        if (a % i == 0 && b % i == 0) {
+            k = i;
+        }
+    }
+    return k;
 }
